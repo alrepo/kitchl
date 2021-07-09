99b290939f91c075d8dfdbbc64ebce0fa46ee94d
tahaddyat <70242053+tahaddyat@users.noreply.github.com>
2021-07-09 10:45:02 +0300

    Update categoriesCarousel.js

--- public/categoriesCarousel.js
+++ public/categoriesCarousel.js
@@ -23,8 +23,8 @@ $('.carousel').slick({
 		{
 			breakpoint: 480,
 			settings: {
-				slidesToShow: 1,
-				slidesToScroll: 1
+				slidesToShow: 2,
+				slidesToScroll: 2
 			}
 		}
 		// You can unslick at a given breakpoint now by adding:
