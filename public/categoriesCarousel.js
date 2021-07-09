f0050b664aad4dda99c58c2797483595623ffa87
tahaddyat <70242053+tahaddyat@users.noreply.github.com>
2021-07-09 13:53:38 +0300

    Update categoriesCarousel.js

--- public/categoriesCarousel.js
+++ public/categoriesCarousel.js
@@ -1,30 +1,37 @@
 $('.carousel').slick({
 	dots: true,
 	infinite: false,
-	speed: 300,
-	slidesToShow: 4,
-	slidesToScroll: 4,
+	speed: 1000,
+	slidesToShow: 6,
+	slidesToScroll: 6,
 	responsive: [{
-			breakpoint: 1024,
+			breakpoint: 1100,
 			settings: {
-				slidesToShow: 3,
-				slidesToScroll: 3,
+				slidesToShow: 6,
+				slidesToScroll: 6,
 				infinite: true,
 				dots: true
 			}
 		},
 		{
+			breakpoint: 800,
+			settings: {
+				slidesToShow: 4,
+				slidesToScroll: 5
+			}
+		},
+		{
 			breakpoint: 600,
 			settings: {
-				slidesToShow: 2,
-				slidesToScroll: 2
+				slidesToShow: 4,
+				slidesToScroll: 4
 			}
 		},
 		{
 			breakpoint: 480,
 			settings: {
-				slidesToShow: 2,
-				slidesToScroll: 2
+				slidesToShow: 4,
+				slidesToScroll: 4
 			}
 		}
 		// You can unslick at a given breakpoint now by adding:
